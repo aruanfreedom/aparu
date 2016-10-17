d689c4cd9c26f3ffb9790b9d1a26749b00166bc8
Aruan.Kapbasov <aruanfreedom@outlook.com>
2016-10-17 20:42:16 +0600

    share

--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ var Aparu = {
 		$(".share-content").on("mouseleave", function () {
 			$(this).hide('slow');
 		});
+		$('#fb').on('click', function() {
+			window.open('https://www.facebook.com/sharer.php?s=100&p[title]=Самый%простой%вызов%машины% -%одной%кнопкой!&p[url]=http://taxi.aparu.kz&p[images][0]=http://taxi.aparu.kz/img/logo-big.png', '_blank');
+		});
+		$('#od').on('click', function() {
+			window.open('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st.comments=Самый%простой%вызов%машины% -%одной%кнопкой!&st._surl=http://taxi.aparu.kz', '_blank');
+		});
+		$('#vk').on('click', function() {
+			window.open('http://vk.com/share.php?url=http://taxi.aparu.kz', '_blank');
+		});
 	}
 };
 
