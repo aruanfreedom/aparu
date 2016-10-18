71337b0544485fc21fa8792963ac7714e2925dad
Aruan.Kapbasov <aruanfreedom@outlook.com>
2016-10-18 15:15:07 +0600

    fb bug

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var Aparu = {
 			$(this).hide('slow');
 		});
 		$('#fb').on('click', function() {
-			window.open('https://www.facebook.com/sharer.php?s=100&p[title]=Самый%простой%вызов%машины% -%одной%кнопкой!&p[url]=http://taxi.aparu.kz&p[images][0]=http://taxi.aparu.kz/img/logo-big.png', '_blank');
+			window.open('http://www.facebook.com/sharer.php?s=100&p[title]=Самый%простой%вызов%машины%-%одной%кнопкой![summary]=Вызывайте%такси%в%приложения%АПАРУ:%для%вызова%машины%достаточно%нажать%одну%кнопку%и%по%вашему%заказу%приедет%наилучший%водитель&p[url]=http://taxi.aparu.kz&p[images][0]=http://taxi.aparu.kz/img/logo-big.png', '_blank');			
 		});
 		$('#od').on('click', function() {
 			window.open('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st.comments=Самый%простой%вызов%машины% -%одной%кнопкой!&st._surl=http://taxi.aparu.kz', '_blank');
